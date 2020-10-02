a7d3c5f29df53d2b0b29676dcdb562f5dbd3e6dc
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2020-10-02 21:44:24 +0300

    Add files via upload

---
+++ Kullanıcıdan veri alıp farklı şekillerde ekrana yazdırma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Surname: ");
+            string surname = Console.ReadLine();
+            Console.Write("Age: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine( "My name is " + name + " " + surname+" and I'm "+age+" years old." );
+            Console.WriteLine( $"My name is {name} {surname} and I'm {age} years old." );
+            Console.WriteLine( string.Format("My name is {0} {1} and I'm {2} years old.",name,surname,age));
+        }
+    }
+}
